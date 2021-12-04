375543dc63b2e61ae05ad09a5f39507047a7e1d1
Sunwarul Islam <sanoarulslm@gmail.com>
2021-12-04 18:11:26 +0600

    str pad and carbon installed

---
+++ how-to/minutes-to-hh-ii.php
@@ -0,0 +1,7 @@
+<?php 
+
+require_once 'vendor/autoload.php';
+use Carbon\Carbon;
+use Carbon\CarbonInterval;
+
+$t1 = Carbon::now('Asia/Dhaka');
---
+++ how-to/str-pad-both-side.php
@@ -0,0 +1,5 @@
+<?php
+
+$str = '0:0';
+// $str = '12:55';
+echo str_pad($str, 5, '0', STR_PAD_BOTH);
