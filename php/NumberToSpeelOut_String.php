a2b0cc2e4e02bbcc4e87cff967536c7aed9d357a
Sunwarul Islam <sanoarulslm@gmail.com>
2022-09-27 13:54:45 +0600

    output shown

--- php/NumberToSpeelOut_String.php
+++ php/NumberToSpeelOut_String.php
@@ -4,8 +4,11 @@ $formatter = new NumberFormatter('en-US', NumberFormatter::SPELLOUT);
 $str = $formatter->format(147570);
 echo ucwords($str);
 
+// Output: One Hundred Forty-seven Thousand Five Hundred Seventy
 
 $bdFormatter = new NumberFormatter('bn-BD', NumberFormatter::CURRENCY);
 
 echo PHP_EOL;
 echo $bdFormatter->format(423431245);
+
+// Output: ৪২,৩৪,৩১,২৪৫.০০৳
